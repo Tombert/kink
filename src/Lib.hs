faa8fa54666a75f0ddfeb165bd0d35e7370b4334
Thomas Gebert <thomas@gebert.sexy>
2017-06-15 08:40:22 -0400

    Get query parameter parsing working correctly.  Rethink best way to stream video with seeking support.

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,6 +4,9 @@ import Network.Wai
 import Data.Conduit.Process as DCP
 import Data.Conduit.Binary as CL
 import Data.Conduit.List as CLS
+import Data.HashMap.Lazy as HML
+import qualified Data.Text as T
+import Data.Maybe as DM
 import Conduit
 import Network.HTTP.Types
 import Data.ByteString as BS 
@@ -15,17 +18,39 @@ import GHC.Word
 
 callback :: Builder -> Flush Builder 
 callback x = Chunk x
-
-app :: Application
-app _ respond = do
+-- -> HashMap BS.ByteString BS.ByteString 
+transcode ::  (Response -> IO ResponseReceived) -> HashMap BS.ByteString BS.ByteString -> IO ResponseReceived
+transcode respond queryParams = do
+    -- let startTime' = HML.lookup "start" queryParams 
+    --     startTime = fromMaybe startTime'
     Prelude.putStrLn "I've done some IO here"
     (Inherited, src, Inherited, cph) <-
-        DCP.streamingProcess (shell "ffmpeg -i /home/tombert/Downloads/sintel.mp4 -f matroska -")
+        DCP.streamingProcess (shell "ffmpeg -i /home/tombert/Downloads/sintel.mp4 -vcodec libx264 -x264-params keyint=60:no-scenecut  -f matroska -")
+        --DCP.streamingProcess (shell "cat /home/tombert/Downloads/sintel.mp4")
                  -- .| mapC BS.concat
 
-    respond $ responseSource status200 [] (src .| mapC BSB.fromByteString .| mapC Chunk) 
+    respond $ responseSource status200 [("Content-Type", "video/webm") ,("X-Content-Duration", "90.0")] (src .| mapC BSB.fromByteString .| mapC Chunk) 
+
+
+filterOptions =
+    DM.mapMaybe (\(key, value)->
+        case value of
+            Just y -> Just (key, y)
+            Nothing -> Nothing
+                            )
+extractQueryParams = (fromList . filterOptions . queryString )
+
+appRouter :: Application
+appRouter request respond = 
+      let fart = queryParams in 
+      case rawPathInfo request of
+          "/transcode" -> transcode respond fart
+
+    where
+        pathparts = pathInfo request
+        queryParams =  extractQueryParams request
 
 someFunc :: IO ()
 someFunc = do
     Prelude.putStrLn $ "http://localhost:8080/"
-    run 8080 app
+    run 8080 appRouter 
